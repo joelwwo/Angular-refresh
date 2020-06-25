5230f2bbfd3421718af4d20994371b00c0beeacd
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2020-06-25 11:31:41 -0300

    Finalização

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-
-import { HEROES } from './mock-heroes';
-import { IHero } from './Interfaces/Hero';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { IHero } from './Interfaces/Hero';
+import { HEROES } from './mock-heroes';
 import { MessagesService } from './messages.service';
 
 
@@ -11,17 +13,95 @@ import { MessagesService } from './messages.service';
 })
 export class HeroService {
 
-  constructor(private messagesService: MessagesService) { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  getHeroes(): Observable<IHero[]> {
-    this.messagesService.addMessage('HeroService: fetched heroes')
-    return of(HEROES)
+  private heroesUrl = 'api/heroes';
+
+  constructor(private messagesService: MessagesService, private http: HttpClient) { }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
+  getHeroes(): Observable<IHero[]> {
+    //return of(HEROES)
+    return this.http.get<IHero[]>(this.heroesUrl).
+      pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<IHero[]>('getHeroes', [])))
+  }
+
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<IHero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messagesService.addMessage(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<IHero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<IHero>(`getHero id=${id}`))
+    );
+  }
+
+  private log(message: string) {
+    this.messagesService.addMessage(`HeroService: ${message}`);
+  }
+
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: IHero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: IHero): Observable<IHero> {
+    return this.http.post<IHero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: IHero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<IHero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: IHero | number): Observable<IHero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<IHero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<IHero>('deleteHero'))
+    );
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<IHero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<IHero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<IHero[]>('searchHeroes', []))
+    );
   }
 
 }
